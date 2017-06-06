0992809b3854cb029870874f834df1c29c76df0b
anakassia <kassia75@gmail.com>
2017-06-06 10:04:23 -0300

    Update relz_cicl.class.php

--- relz_cicl.class.php
+++ relz_cicl.class.php
@@ -3,21 +3,13 @@
 	private $idRcc;
 	private $pontuacaoAlcancadaRcc;
 	
-	public function getIdRcc(){
-		return $this->idRcc;
+	
+	public function __set($atrib, $value){
+		$this -> $atrib = $valeu;
 	}
 	
-	public function getPontuacaoAlcancadaRcc(){
-		return $this->pontuacaoAlcancadaRcc;
-	}
-	
-
-	public function setIdRcc(){
-		return $this->idRcc;
-	}
-	
-	public function setPontuacaoAlcancadaRcc(){
-		return $this->pontuacaoAlcancadaRcc;
-	}
+	public function __get($atrib){
+		return $this -> $atrib;
+		}
 
 ?>
